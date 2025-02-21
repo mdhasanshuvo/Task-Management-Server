59f768324aad53859c2d115922f1e93830be6a19
mdhasanshuvo <mdhasanshuvo220@gmail.com>
2025-02-22 03:56:57 +0600

    User info post api created

--- index.js
+++ index.js
@@ -33,6 +33,20 @@ async function run() {
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const userCollection = client.db("Task-Manager-DB").collection("Users");
+
+    app.post("/users", async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email };
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+            return res.send({ message: "user already exists", insertedId: null });
+        }
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+    });
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
